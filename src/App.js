87c6d155424e35f60872342a6807f0e6e3f72058
shashidharnaik <shashidhar.n@triofi.in>
2022-11-30 01:10:51 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap';
-import Appbar from './components/Appbar';
-import Home from './components/Home';
-import TopBrands from './components/TopBrands';
-import Footer from './components/Footer';
-import About from './components/About';
-import Products from './components/Products';
-import OurBrands from './components/OurBrands';
-import ContactUs from './components/ContactUs';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap";
+import Appbar from "./components/Appbar";
+import Home from "./components/Home";
+import TopBrands from "./components/TopBrands";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Products from "./components/Products";
+import OurBrands from "./components/OurBrands";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Appbar />
       <Home />
       <TopBrands />
